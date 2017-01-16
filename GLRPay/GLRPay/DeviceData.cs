41ac8f84f17a1300f0cc579c4a9d4f3096ce5873
jurjurijur <jurrie-99@hotmail.com>
2017-01-16 17:05:56 +0100

    classes added

---
+++ GLRPay/GLRPay/DeviceData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace GLRPayMachine
+{
+    public class DeviceData
+    {
+        public string UniqueID { get; private set; }
+        public DeviceType Device { get; private set; } = DeviceType.Card;
+        public IntentType Intent { get; private set; }
+
+        public DeviceData(byte[] RawDeviceType, byte[] RawStationMode,string UID)
+        {
+            UniqueID = UID;
+            int Devicenum = 0;
+            if(int.TryParse(Encoding.UTF8.GetString(RawDeviceType),out Devicenum))
+            {
+                Device = (DeviceType)Devicenum;
+            }else
+            {
+                throw new InvalidCastException("the device type was not a number!");
+            }
+            
+
+            if(Device == DeviceType.Smartphone)
+            {
+                int intentnum = 0;
+                if(int.TryParse(Encoding.UTF8.GetString(RawDeviceType), out intentnum))
+                {
+                    Intent = (IntentType)intentnum;
+                }else
+                {
+                    throw new InvalidCastException("the intent was not a number!");
+                }
+                
+            }
+        }
+    }
+}
---
+++ GLRPay/GLRPay/GLRPayFunctions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLRPayMachine
+{
+    class GLRPayFunctions
+    {
+        Stationmode DeviceMode;
+        private DeviceData Data;
+
+        const string ValidGeneralId = "0a8a35a8fe49788a9c3d738411090ac0";
+        bool validate = false;
+
+        // function to check if card is supported
+        private bool Validate(string[] RawGeneralID)
+        {
+            if (StringConverter(RawGeneralID) == ValidGeneralId)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        // function to make sure te connection gets established
+        private void Handshake(string MyUid, int MyStation, int ThereStation, string ThereUid)
+        {
+
+        }
+        // function to receive currency
+        private void Load()
+        {
+
+        }
+        // function to send currency
+        private void Pay()
+        {
+
+        }
+        // function to upload to the database
+        private void UploadDatabase()
+        {
+
+        }
+
+        // convert a stringarray to a single string
+        private string StringConverter(string[] StringArray)
+        {
+            string FullString = "";
+            foreach (string element in StringArray)
+            {
+                FullString += element;
+            }
+            return FullString;
+        }
+    }
+
+    public enum Stationmode
+    {
+        PayStation,
+        LoadStation,
+    }
+    public enum IntentType
+    {
+
+        Pay,
+        Charge
+    }
+
+    public enum DeviceType
+    {
+        Card,
+        Smartphone
+    }
+}
+
